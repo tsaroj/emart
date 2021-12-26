6335b1e790bfa1b9a1b07799629c4d4c0471867d
Saroj Guragain <saroj073bct@ioepc.edu.np>
2021-12-26 12:37:31 +0545

    update in products components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,6 @@ import { Products } from './Products'
 
 export const Home = () => {
     return (
-<<<<<<< Updated upstream
         <>
             <div className='hero'>
 
@@ -16,17 +15,6 @@ export const Home = () => {
                             <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASONS ARRIVALS</h5>
                             <p className="card-text fs-2">CHECK OUT ALL TRENDS</p>
                         </div>
-=======
-        <div className='hero'>
-
-            <div className="card bg-dark text-white border-0">
-                <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px" />
-                <div className="card-img-overlay d-flex flex-column justify-content-center">
-                    <div className='container'>
-                        <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASONS ARRIVALS</h5>
-                        <p className="card-text fs-2">CHECK OUT ALL TRENDS</p>
-
->>>>>>> Stashed changes
                     </div>
                 </div>
                 <Products />
