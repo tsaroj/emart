8d3f43e4317f22b289c91d1397db8329e5dc1ec0
Saroj Guragain <saroj073bct@ioepc.edu.np>
2021-12-26 12:58:19 +0545

    update in title name in product component

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -50,7 +50,7 @@ export const Products = () => {
                                 <div className="card h-100 text-center p-4 " key={product.id}>
                                     <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
                                     <div className="card-body">
-                                        <h5 className="card-title mb-0">{product.title}</h5>
+                                        <h5 className="card-title mb-0">{product.title.substring(0, 20)}...</h5>
                                         <p className="card-text lead fw-bold">${product.price}</p>
                                         <a href="#" className="btn btn-outline-dark">Buy Now</a>
                                     </div>
