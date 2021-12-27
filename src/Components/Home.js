4ac2105f9a7514623b5371cf681d0e713e285e3a
Saroj Guragain <saroj073bct@ioepc.edu.np>
2021-12-27 20:07:50 +0545

    Create Home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div className='hero'>
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="550px" />
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className='container'>
+                        <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASONS ARRIVALS</h5>
+                        <p class="card-text fs-2">CHECK OUT ALL TRENDS</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
