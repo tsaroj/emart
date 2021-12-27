4fd3f2ba45c62fcdbc30ef38f79a6694a851566f
Saroj Guragain <saroj073bct@ioepc.edu.np>
2021-12-27 11:12:00 +0545

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Home } from './Components/Home';
 import { Navbar } from './Components/Navbar';
+import { Routes, Route } from 'react-router-dom'
+import { Products } from './Components/Products';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route end path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
     </>
   );
 }
